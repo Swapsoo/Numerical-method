59e22dd29eae9b691ac37710fb1f0aeadf0b8ca2
Swapsoo <134841328+Swapsoo@users.noreply.github.com>
2023-07-03 12:43:13 +0545

    Add files via upload

---
+++ Simpson's rule.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+float f(float x)
+{
+
+    return (1-exp(-x/2.0));
+
+}
+void main()
+{
+
+    float a,b,h,x,ans,sum=0;
+    int n,i;
+    printf("Enter initial and final value of x: \n");
+    scanf("%f%f",&a,&b);
+
+    printf("Number of segments: ");
+    scanf("%d",&n);
+
+    h=(b-a)/n;
+
+    for(i=1;i<n;i++)
+    {
+
+        x=a+i*h;
+        if(i%2==0)
+        {
+
+            sum=sum+2*f(x);
+        }
+        else
+        {
+           sum=sum+4*f(x);
+
+        }
+    }
+    ans=(h/3)*(f(a)+f(b)+sum);
+
+    printf("\n Integral value of f(x)=%f",ans);
+    getch();
+}
